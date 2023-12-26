59de1a44fa1e4fabf0c3bed1c6c4f83cfe3cf74c
Biswash <spikeystona@gmail.com>
2023-12-26 15:12:37 +0545

    isActive

--- 03 Motion Ex 1/src/components/Navbar.tsx
+++ 03 Motion Ex 1/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function Navbar(): JSX.Element {
   return (
@@ -6,19 +6,19 @@ export default function Navbar(): JSX.Element {
       <nav className="bg-teal-500 p-6">
         <ul className="flex justify-center items-center gap-3 ">
           <li className="cursor-pointer hover:underline decoration-wavy	">
-            <Link to="/">Home</Link>
+            <NavLink to="/" className={({isActive}) => isActive ? `text-zinc-500 ` : `text-black`} >Home</NavLink>
           </li>
           <li className="cursor-pointer hover:underline decoration-wavy	">
-            <Link to="/scrollanimation">Scroll Animations</Link>
+            <NavLink to="/scrollanimation" className={({isActive}) => isActive ? `text-zinc-500 ` : `text-black`} >Scroll Animations</NavLink>
+          </li>
+          <li className={"cursor-pointer hover:underline decoration-wavy	"}>
+            <NavLink to="/cursor" className={({isActive}) => isActive ? `text-zinc-500 ` : `text-black`} >Cursor</NavLink>
           </li>
           <li className="cursor-pointer hover:underline decoration-wavy	">
-            <Link to="/cursor">Cursor</Link>
+            <NavLink to="/view" className={({isActive}) => isActive ? `text-zinc-500 ` : `text-black`} >View</NavLink>
           </li>
           <li className="cursor-pointer hover:underline decoration-wavy	">
-            <Link to="/view">View</Link>
-          </li>
-          <li className="cursor-pointer hover:underline decoration-wavy	">
-            <Link to="/empty">Empty</Link>
+            <NavLink to="/empty" className={({isActive}) => isActive ? `text-zinc-500 ` : `text-black`} >Empty</NavLink>
           </li>
         </ul>
       </nav>
